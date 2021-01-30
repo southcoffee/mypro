687a66eb6e0d1591d1c0e869d0f348d368695c34
commyit <com_myit@126.com>
2021-01-30 09:45:46 +0800

    v2

---
+++ src/main/java/com/myit/pojo/Dept.java
@@ -0,0 +1,13 @@
+package com.myit.pojo;
+
+public class Dept {
+    String  name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
